bf49057d0912ab7b1a1b504cc804ca62422fb84b
Prakhar Mani Tripathi <prakhar.manitripathi.cd.mec20@itbhu.ac.in>
2023-06-15 14:41:17 +0530

    updated some files

--- codeprakhar/nasa_image_api.sh
+++ codeprakhar/nasa_image_api.sh
@@ -1,3 +1,3 @@
-data=$(curl --location 'https://api.nasa.gov/planetary/apod?api_key={{ secrets.API_KEY_NASA }}')
-url=(echo "$data" | jq -r .hdurl)
+data=$(curl -s --location 'https://api.nasa.gov/planetary/apod?api_key={{ secrets.API_KEY_NASA }}')
+url=$(echo "$data" | jq -r '.hdurl')
 echo "$url"
