e1590cff462f1dd7dc1bfa9d54a50e8664afad97
Prakhar Mani Tripathi <prakhar.manitripathi.cd.mec20@itbhu.ac.in>
2023-06-15 14:45:55 +0530

    updated some files

--- codeprakhar/powerstart.sh
+++ codeprakhar/powerstart.sh
@@ -1,7 +1,7 @@
 echo "Press 1 to get an idea for your bucket list"
 echo "Press 2 to download a HD Astronomy image by NASA"
 read choice
-if(( $choice==1 ))
+if[ "$choice" -eq 1 ]
     then 
         bash codeprakhar/bucket_list_api.sh
     else
